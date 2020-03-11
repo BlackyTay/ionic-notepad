3d9d54042bc1b0a809d888e5fb01a449f90f92ac
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-11 14:30:21 +0800

    Add detail page function

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotesService } from '../services/notes.service';
+import { Note } from '../interfaces/note';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+  public note: Note;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private notesService: NotesService) { 
+    // placeholder
+    this.note = {
+     id: '',
+     title: '',
+     content: '' 
+    };
+   }
 
   ngOnInit() {
+    // Get id from url
+    let noteId = this.route.snapshot.paramMap.get('id');
+
+    // Checking the data is loaded before getting the note
+    if(this.notesService.loaded){
+      this.note = this.notesService.getNote(noteId);
+    } else {
+      this.notesService.load().then(() => {
+        this.note = this.notesService.getNote(noteId);
+      });
+    }
+  }
+
+  noteChanged() {
+    this.notesService.save();
+  }
+
+  deleteNote() {
+    this.notesService.deleteNote(this.note);
+    this.router.navigate(['./notes']);
   }
 
 }
