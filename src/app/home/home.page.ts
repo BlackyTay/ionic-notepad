9001525cf997552533f53d1b6a06efa35c20d103
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-11 13:58:30 +0800

    Add alert for new note

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { NotesService } from '../services/notes.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,35 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage implements OnInit{
 
-  constructor(private alertCtrl: AlertController) {}
+  constructor(private notesService: NotesService, private alertCtrl: AlertController) {}
 
   ngOnInit() {
-    
+    this.notesService.load();
+  }
+
+  addNote() {
+    this.alertCtrl.create({
+      header: 'New Note',
+      message: 'What should the title of this note be?',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.notesService.createNote(data.title);
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    });
   }
 }
